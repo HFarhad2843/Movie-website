6ce928a52e659698c5a560a5b8f1d7ca8251a9d8
HFarhad2843 <146637915+HFarhad2843@users.noreply.github.com>
2024-02-27 14:20:34 +0400

    first commit

---
+++ movies.js
@@ -0,0 +1,40 @@
+let row=document.querySelector('.row')
+
+fetch('https://api.tvmaze.com/shows')
+  .then(response => {
+   
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+   
+    return response.json();
+  })
+  .then(data => {
+   
+    data.forEach(movie => {
+        row.innerHTML +=`<div class="card col-3">
+        <img src="${movie.image.medium}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${movie.name}</h5>
+          <p class="card-text">Premiere:${movie.premiered}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">IMDB:${movie.rating.average}</li>
+          <li class="list-group-item">Genre:${movie.genres}</li>
+          <li class="list-group-item">Language:${movie.language}</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Go to Website</a>
+          <a href="#" class="card-link">Go to detail</a>
+        </div>
+        </div>`
+    });
+ 
+    
+  })
+  .catch(error => {
+  
+    console.error('There was a problem with the fetch operation:', error);
+  });
+  
+  
